0da3723f0728da94a3a47093d74a5dd9c8d15dc3
Miguel Ramos <miguel.marques.ramos@gmail.com>
2019-02-16 21:38:15 +0000

    test(utils): add tests for monitor cpu/mem

---
+++ packages/soldier/__tests__/utils.spec.ts
@@ -0,0 +1,24 @@
+import { getCpuTimes, getCpuUsage, getMemoryUsage } from '../lib/utils';
+
+describe(' > Utils', () => {
+  it('# Should get cpu times', () => {
+    const cpuTimes = getCpuTimes();
+
+    expect(cpuTimes.idle > 0).toBeTruthy();
+    expect(cpuTimes.nice >= 0).toBeTruthy();
+    expect(cpuTimes.sys > 0).toBeTruthy();
+    expect(cpuTimes.user > 0).toBeTruthy();
+  });
+
+  it('# Should get cpu percent', () => {
+    const cpuUsage = getCpuUsage();
+
+    expect(cpuUsage > 0).toBeTruthy();
+  });
+
+  it('# Should get memory percent', () => {
+    const memUsage = getMemoryUsage();
+
+    expect(memUsage > 0).toBeTruthy();
+  });
+});
